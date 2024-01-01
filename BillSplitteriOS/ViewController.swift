2e2bcb0e82f7f3608d3b95a2280e954a65460b2c
Temur <timurzaitov1998@gmail.com>
2024-01-01 18:33:55 +0100

    Initial Commit

---
+++ BillSplitteriOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BillSplitteriOS
+//
+//  Created by Temur on 01/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
